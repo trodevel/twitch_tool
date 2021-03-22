2ceb48a1df17619555eec9ad41a28ef66d7d1711
trodevel <trodevel@gmail.com>
2021-03-22 20:05:50 +0100

    working version

--- twitch_tool.py
+++ twitch_tool.py
@@ -231,7 +231,7 @@ def determine_users_in_category( driver, parent, category_name ):
     trigger = helpers.find_element_by_xpath_with_timeout( parent, "//div[@class='scrollable-trigger__wrapper']", 10 )
     #trigger = driver.find_element_by_css_selector( "div[class='scrollable-trigger__wrapper']" )
     #ActionChains(driver).move_to_element( trigger ).perform()
-    driver.execute_script( "document.getElementsByClassName('scrollable-trigger__wrapper').scrollIntoView();" )
+    driver.execute_script( "document.getElementsByClassName('scrollable-trigger__wrapper')[0].scrollIntoView();" )
     #driver.execute_script( "argument[0].scrollIntoView();", trigger )
 
     #trigger.send_keys( Keys.ARROW_DOWN )
