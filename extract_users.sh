be62143b53fab898fa3d041ba666cdeaf124d0fc
trodevel <trodevel@gmail.com>
2021-03-28 04:06:39 +0200

    output refinement

--- extract_users.sh
+++ extract_users.sh
@@ -11,4 +11,11 @@ filename="${filenamea%.*}"
 
 FL_O=${filename}_users.csv
 
+echo "input  = $FL"
+echo "output = $FL_O"
+
 grep -o 'data-username="[a-z0-9_]*"' $FL | grep -o '".*"' | sed 's/"//g' > $FL_O
+
+lines=$( cat $FL_O | wc -l )
+
+echo "INFO: $lines lines written to $FL_O"
